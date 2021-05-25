1a77309238a7f25515ee894c0496d1140c723849
TamMinh <tamminh2003@gmail.com>
2021-05-25 11:43:59 +1000

    removed refresh button, added caemdarSizeModule, modified buildDaysGrid, modified show

--- working.js
+++ working.js
@@ -13,7 +13,8 @@ function documentCompleteHandler() {
     }
     )(window);
 
-    // ------------------------------------------
+
+    // --------Calendar--------------------------
     (function (window) {
 
       class Calendar {
@@ -230,11 +231,6 @@ function documentCompleteHandler() {
           });
         }
 
-        // @ Description: Generate Refresh button to run refresh view method
-        createRefreshButton() {
-
-        }
-
         // * ================================
 
 
@@ -368,22 +364,6 @@ function documentCompleteHandler() {
             }));
 
           showUserTaskButton.addEventListener('click', this.showUserTaskButtonHandler);
-          // * -----------------------------------------
-
-          // * Add REFRESH button ---------------------------
-          let refreshButton = filterContainer.querySelector('.aug-refresh-button');
-
-          if (!refreshButton)
-            refreshButton = filterContainer.appendChild(BX.create('button', {
-              attrs: {
-                class: "aug-refresh-button ui-btn ui-btn-themes ui-btn-xs ui-btn-primary ui-btn-round"
-              },
-              text: 'Refresh'
-            }));
-
-          refreshButton.addEventListener('click', e => {
-            this.refreshCalendarDisplay();
-          });
           // * -----------------------------------------
         }
 
@@ -531,51 +511,38 @@ function documentCompleteHandler() {
         }
 
         // @ Description: Extend calendar cell and entry holder
-        calendarExtend() {
-          // Build number of slot selector
+        calendarSizeModule() {
+          // new buildDaysGrid
+          this.assignAUGbuildDaysGrid();
+          this.assignAUGshow();
+
+          // Generate Size Selector
           let wrap = document.querySelector('.calendar-view-switcher-list');
-          // TODO a dropdown list where user can select the size of the calendar.
 
-          let selector = wrap.appendChild(BX.create('select',
-            {
-              attrs: {
-                name: 'cellSize',
-                'id': 'size-selector'
-              }
-            }
+          wrap.appendChild(BX.create('label',
+            { attrs: { for: 'calSize', class: 'calendar-counter-title' }, text: 'Calendar Size:' }
           ));
 
-          selector.appendChild(BX.create('option',
-            { 
-              attrs: { value: 'test1' }, 
-              text: 'test1' 
+          let selectTag = BX.create('select', { attrs: { name: 'calSize', d: 'calSize' } });
+          selectTag.appendChild(BX.create('option', { attrs: { value: 'compact' }, text: 'Compact' }));
+          selectTag.appendChild(BX.create('option', { attrs: { value: 'comfort' }, text: 'Comfortable' }));
+          selectTag.appendChild(BX.create('option', { attrs: { value: 'medium' }, text: 'Medium' }));
+          wrap.appendChild(selectTag);
+          // ---- 
+
+          // Handler
+          selectTag.addEventListener('change', e => {
+            let monthView = this.calendar.getView('month');
+            if (e.target.value == 'compact') {
+              monthView.rowHeight = 144; monthView.slotHeight = 20;
+            } else if (e.target.value == 'comfort') {
+              monthView.rowHeight = 664; monthView.slotHeight = 40;
+            } else if (e.target.value == 'medium') {
+              monthView.rowHeight = 464; monthView.slotHeight = 40;
             }
-          ));
-
-          selector.appendChild(BX.create('option',
-            { 
-              attrs: { value: 'test2' }, 
-              text: 'test2' 
-            }
-          ));
-
-
-          // Extend MonthView
-          let monthView = this.calendar.getView("month");
-
-          monthView.rowHeight = 500;
-          monthView.slotHeight = 40;
-          let cellHeight = 33;
-
-          monthView.slotsCount = Math.floor((monthView.rowHeight - monthView.eventHolderTopOffset) / monthView.slotHeight);
-
-          monthView.monthRows.forEach(c => {
-            c.style.height = `${monthView.rowHeight}px`;
+            monthView.slotsCount = Math.floor((this.rowHeight - this.eventHolderTopOffset) / this.slotHeight);
+            monthView.show();
           });
-
-          // monthView.displayEntries();
-
-          document.querySelectorAll('.calendar-event-line-text').forEach(c => c.style.lineHeight = `${cellHeight}px`);
         }
 
         // ! INJECTED METHODS
@@ -1105,6 +1072,95 @@ function documentCompleteHandler() {
           }
         }
 
+        assignAUGbuildDaysGrid() {
+          window.BXEventCalendar.instances[Object.keys(BXEventCalendar.instances)[0]]
+            .getView('month').buildDaysGrid = function (params) {
+              if (!params)
+                params = {};
+
+              var
+                i, dayOffset,
+                grid = params.grid || this.grid,
+                viewRangeDate = this.calendar.getViewRangeDate(),
+                year = viewRangeDate.getFullYear(),
+                month = viewRangeDate.getMonth(),
+                height = this.util.getViewHeight(),
+                displayedRange = BX.clone(this.getViewRange(), true),
+                date = new Date();
+
+              BX.cleanNode(grid);
+              date.setFullYear(year, month, 1);
+
+              this.dayIndex = {};
+              this.days = [];
+              this.entryHolders = [];
+
+              this.currentMonthRow = false;
+              this.monthRows = [];
+
+              if (this.util.getWeekStart() != this.util.getWeekDayByInd(date.getDay())) {
+                dayOffset = this.util.getWeekDayOffset(this.util.getWeekDayByInd(date.getDay()));
+                date.setDate(date.getDate() - dayOffset);
+
+                displayedRange.start = new Date(date.getTime());
+                displayedRange.start.setHours(0, 0, 0, 0);
+
+                for (i = 0; i < dayOffset; i++) {
+                  this.buildDayCell({ date: date, month: 'previous', grid: grid });
+                  date.setDate(date.getDate() + 1);
+                }
+              }
+
+              date.setFullYear(year, month, 1);
+              while (date.getMonth() == month) {
+                this.buildDayCell({ date: date, grid: grid });
+                date.setDate(date.getDate() + 1);
+              }
+
+              if (this.util.getWeekStart() != this.util.getWeekDayByInd(date.getDay())) {
+                dayOffset = this.util.getWeekDayOffset(this.util.getWeekDayByInd(date.getDay()));
+                date.setFullYear(year, month + 1, 1);
+                for (i = dayOffset; i < 7; i++) {
+                  this.buildDayCell({ date: date, month: 'next', grid: grid });
+                  date.setDate(date.getDate() + 1);
+                }
+
+                displayedRange.end = new Date(date.getTime());
+                displayedRange.end.setHours(23, 59, 59, 59);
+              }
+
+              this.calendar.setDisplayedViewRange(displayedRange);
+
+              // Adjusting rows height to the height of the view
+              if (this.monthRows.length > 0) {
+                this.slotsCount = Math.floor((this.rowHeight - this.eventHolderTopOffset) / this.slotHeight);
+                for (i = 0; i < this.monthRows.length; i++) {
+                  this.monthRows[i].style.height = this.rowHeight + 'px';
+                }
+              }
+            };
+        }
+
+        assignAUGshow() {
+          BXEventCalendar.instances[Object.keys(BXEventCalendar.instances)[0]].getView("month").show =
+
+            function () {
+              window.BXEventCalendarView.prototype.show.apply(this, arguments);
+
+              this.buildDaysTitle();
+              this.buildDaysGrid();
+
+              if (this.calendar.navCalendar)
+                this.calendar.navCalendar.hide();
+
+              this.displayEntries();
+
+              this.calendar.initialViewShow = false;
+              this.gridWrap.style.height = '';
+              this.gridWrap.style.overflow = '';
+            };
+        }
+
         // @ Description: injected method into displayEntries of each view
         //              : _this parameter is to pass the current context 
         //              : (which is the context of displayEntries) into this method.
@@ -1151,7 +1207,7 @@ function documentCompleteHandler() {
 
 
 
-    // ------------------------------------------
+    // ------Utility--------------------------
     (function (window) {
       let Utility = {
 
@@ -1191,7 +1247,7 @@ function documentCompleteHandler() {
 
 
 
-    // ------------------------------------------
+    // -------Observer------------------------------
     (function (window) {
 
       let Observer = {
@@ -1615,7 +1671,7 @@ function documentCompleteHandler() {
     AUG.Observer.startMutationObserver();
 
     // Extend Calendar Cell
-    // window.AUG.Calendar.calendarExtend();
+    AUG.Calendar.calendarSizeModule();
     // ------------------------------------------
 
   } catch (error) {
