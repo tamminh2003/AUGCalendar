a5597d1c39f267366b9f62889d4799fbb4210841
TamMinh <tamminh2003@gmail.com>
2021-05-25 15:52:24 +1000

    update layout

--- CalendarLayer.js
+++ CalendarLayer.js
@@ -1,30 +1,30 @@
 class CalendarLayer {
-    constructor(name, childCalendar, parentCalendar, id) {
-        this.name = name;
-        this.childCalendar = childCalendar;
-        this.parentCalendar = parentCalendar;
+  constructor(name, childCalendar, parentCalendar, id) {
+    this.name = name;
+    this.childCalendar = childCalendar;
+    this.parentCalendar = parentCalendar;
 
-        this.id = id;   // id will link the CalendarLayer to correct Calendar Section
-                        // null if does not link to Calendar Section
-        
-        this.calendar = placeholder; // LINK this to BX calendar instance
-    }
+    this.id = id;   // id will link the CalendarLayer to correct Calendar Section
+    // null if does not link to Calendar Section
 
-    getName() {
-        return this.name;
-    }
-    
-    getChildCalendar() {
-        return this.childCalendar;
-    }
+    this.calendar = placeholder; // LINK this to BX calendar instance
+  }
 
-    getParentCalendar() {
-        return this.parentCalendar;
-    }
+  getName() {
+    return this.name;
+  }
 
-    getId() {
-        return this.id;
-    }
+  getChildCalendar() {
+    return this.childCalendar;
+  }
+
+  getParentCalendar() {
+    return this.parentCalendar;
+  }
+
+  getId() {
+    return this.id;
+  }
 
 
 }
@@ -62,8 +62,8 @@ CalendarTree.push(new CalendarLayer("Melaka", [], ["Malaysia"], placeholder));
 CalendarTree.push(new CalendarLayer("Nilai", [], ["Malaysia"], placeholder));
 CalendarTree.push(new CalendarLayer("Penang", [], ["Malaysia"], placeholder));
 
-CalendarTree.push(new CalendarLayer("Malaysia", ["Ipoh", "Johor Bahru", "Kota Kinabalu", "Kuala Lumpur", 
-                                                "Kuantan", "Kuching", "Melaka", "Nilai", "Penang"], ["Global"], placeholder));
+CalendarTree.push(new CalendarLayer("Malaysia", ["Ipoh", "Johor Bahru", "Kota Kinabalu", "Kuala Lumpur",
+  "Kuantan", "Kuching", "Melaka", "Nilai", "Penang"], ["Global"], placeholder));
 
 CalendarTree.push(new CalendarLayer("Manila", [], ["Philipines"], placeholder));
 
@@ -73,135 +73,135 @@ CalendarTree.push(new CalendarLayer("Singapore", [], ["Singapore"], placeholder)
 
 CalendarTree.push(new CalendarLayer("Singapore", ["Singapore"], ["Global"], placeholder));
 
-CalendarTree.push(new CalendarLayer("Global", ["Australia","China","Indonesia","Malaysia","Philipines","Singapore"], [], placeholder));
+CalendarTree.push(new CalendarLayer("Global", ["Australia", "China", "Indonesia", "Malaysia", "Philipines", "Singapore"], [], placeholder));
 
 
 let CalendarLevel = {
-    Global: {
-        childCalendar: [Australia,
-                        China,
-                        Indonesia,
-                        Malaysia,
-                        Philipines,
-                        Singapore],
-        parentCalendar: []
-    },
-    Australia: {
-        childCalendar: [Adelaide,
-                        Brisbane,
-                        Melbourne,
-                        Perth,
-                        Sydney],
-        parentCalendar: [Global]
-    },
-    China: {
-        childCalendar: [Beijing,
-                        HongKong],
-        parentCalendar: [Global]
-    },
-    Indonesia: {
-        childCalendar: [Bandung,
-                        Jakarta,
-                        Surabaya],
-        parentCalendar: [Global]
-    },
-    Malaysia: {
-        childCalendar: [Ipoh,
-                        Johor,
-                        Kota,
-                        Kuala,
-                        Kuching,
-                        Melaka,
-                        Nilai,
-                        Penang],
-        parentCalendar: [Global]
-    },
-    Philipines: {
-        childCalendar: [Manila],
-        parentCalendar: [Global]
-    },
-    Singapore: {
-        childCalendar: [],
-        parentCalendar: [Global]
-    },
-    Melbourne: {
-        childCalendar: [],
-        parentCalendar: [Australia]
-    },
-    Sydney: {
-        childCalendar: [],
-        parentCalendar: [Australia]
-    },
-    Perth: {
-        childCalendar: [],
-        parentCalendar: [Australia]
-    },
-    Adelaide: {
-        childCalendar: [],
-        parentCalendar: [Australia]
-    },
-    Brisbane: {
-        childCalendar: [],
-        parentCalendar: [Australia]
-    },
-    BeiJing: {
-        childCalendar: [],
-        parentCalendar: [China]
-    },
-    HongKong: {
-        childCalendar: [],
-        parentCalendar: [China]
-    },
-    Bandung: {
-        childCalendar: [],
-        parentCalendar: [Indonesia]
-    },
-    Jakarta: {
-        childCalendar: [],
-        parentCalendar: [Indonesia]
-    },
-    Surabaya: {
-        childCalendar: [],
-        parentCalendar: [Indonesia]
-    },
-    Ipoh: {
-        childCalendar: [],
-        parentCalendar: [Malaysia]
-    },
-    Kota: {
-        childCalendar: [],
-        parentCalendar: [Malaysia]
-    },
-    Kuala: {
-        childCalendar: [],
-        parentCalendar: [Malaysia]
-    },
-    Kuantan: {
-        childCalendar: [],
-        parentCalendar: [Malaysia]
-    },
-    Kuching: {
-        childCalendar: [],
-        parentCalendar: [Malaysia]
-    },
-    Melaka: {
-        childCalendar: [],
-        parentCalendar: [Malaysia]
-    },
-    Nilai: {
-        childCalendar: [],
-        parentCalendar: [Malaysia]
-    },
-    Penang: {
-        childCalendar: [],
-        parentCalendar: [Malaysia]
-    },
-    Johor: {
-        childCalendar: [],
-        parentCalendar: [Malaysia]
-    },
-    Manila: {
-        childCalendar: [],
-        parentCalendar: [Philipines]
-    }
+  Global: {
+    childCalendar: [Australia,
+      China,
+      Indonesia,
+      Malaysia,
+      Philipines,
+      Singapore],
+    parentCalendar: []
+  },
+  Australia: {
+    childCalendar: [Adelaide,
+      Brisbane,
+      Melbourne,
+      Perth,
+      Sydney],
+    parentCalendar: [Global]
+  },
+  China: {
+    childCalendar: [Beijing,
+      HongKong],
+    parentCalendar: [Global]
+  },
+  Indonesia: {
+    childCalendar: [Bandung,
+      Jakarta,
+      Surabaya],
+    parentCalendar: [Global]
+  },
+  Malaysia: {
+    childCalendar: [Ipoh,
+      Johor,
+      Kota,
+      Kuala,
+      Kuching,
+      Melaka,
+      Nilai,
+      Penang],
+    parentCalendar: [Global]
+  },
+  Philipines: {
+    childCalendar: [Manila],
+    parentCalendar: [Global]
+  },
+  Singapore: {
+    childCalendar: [],
+    parentCalendar: [Global]
+  },
+  Melbourne: {
+    childCalendar: [],
+    parentCalendar: [Australia]
+  },
+  Sydney: {
+    childCalendar: [],
+    parentCalendar: [Australia]
+  },
+  Perth: {
+    childCalendar: [],
+    parentCalendar: [Australia]
+  },
+  Adelaide: {
+    childCalendar: [],
+    parentCalendar: [Australia]
+  },
+  Brisbane: {
+    childCalendar: [],
+    parentCalendar: [Australia]
+  },
+  BeiJing: {
+    childCalendar: [],
+    parentCalendar: [China]
+  },
+  HongKong: {
+    childCalendar: [],
+    parentCalendar: [China]
+  },
+  Bandung: {
+    childCalendar: [],
+    parentCalendar: [Indonesia]
+  },
+  Jakarta: {
+    childCalendar: [],
+    parentCalendar: [Indonesia]
+  },
+  Surabaya: {
+    childCalendar: [],
+    parentCalendar: [Indonesia]
+  },
+  Ipoh: {
+    childCalendar: [],
+    parentCalendar: [Malaysia]
+  },
+  Kota: {
+    childCalendar: [],
+    parentCalendar: [Malaysia]
+  },
+  Kuala: {
+    childCalendar: [],
+    parentCalendar: [Malaysia]
+  },
+  Kuantan: {
+    childCalendar: [],
+    parentCalendar: [Malaysia]
+  },
+  Kuching: {
+    childCalendar: [],
+    parentCalendar: [Malaysia]
+  },
+  Melaka: {
+    childCalendar: [],
+    parentCalendar: [Malaysia]
+  },
+  Nilai: {
+    childCalendar: [],
+    parentCalendar: [Malaysia]
+  },
+  Penang: {
+    childCalendar: [],
+    parentCalendar: [Malaysia]
+  },
+  Johor: {
+    childCalendar: [],
+    parentCalendar: [Malaysia]
+  },
+  Manila: {
+    childCalendar: [],
+    parentCalendar: [Philipines]
+  }
 }
